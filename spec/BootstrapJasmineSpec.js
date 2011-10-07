60c8cce8872ed7215c30415821faf7d9bb39289e
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-10-07 16:29:50 +0200

    BDD Bootstrapped
    BDD is bootstrapped by providing a Bootstrap function which is
    specified.

---
+++ spec/BootstrapJasmineSpec.js
@@ -0,0 +1,5 @@
+describe("Jasmine", function() {
+	it("is configured correctly", function(){
+		expect(bootstrap()).toEqual("Jasmine is bootstrapped");
+	});
+});
---
+++ src/BootstrapJasmine.js
@@ -0,0 +1,3 @@
+function bootstrap() {
+	return "Jasmine is bootstrapped";
+}
